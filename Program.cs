2c1ba513b4ec35f945e9350a87fc1bb955af2069
Moin <moin.mahmud38@gmail.com>
2025-03-22 09:06:03 -0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -19,7 +19,7 @@ builder.Services.AddCors(options =>
         policy =>
         {
             // policy.WithOrigins("http://localhost:4200") // Replace with the Angular app's URL
-            policy.WithOrigins("https://white-rock-0f754d01e.6.azurestaticapps.net/") // Replace with the Angular app's URL
+            policy.WithOrigins("https://white-rock-0f754d01e.6.azurestaticapps.net") // Replace with the Angular app's URL
                   .AllowAnyHeader()
                   .AllowAnyMethod();  
         });
